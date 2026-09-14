0795e20c7978452768276f3cfebc87ad1a26a379
jhonatanparada499 <jhonatanparada499@gmail.com>
2026-09-14 09:14:57 -0400

    feat: add vector initializers and func calls

--- Week-03/practice.cpp
+++ Week-03/practice.cpp
@@ -26,5 +26,21 @@ int main(){
   cout << "Larger double: " << largeOf(d1,d2) << endl;
   cout << "Larger string: " << largeOf(s1,s2) << endl;
 
+  // vectors
+  vector<int> v1 = {1,2,3};
+  vector<int> v2(5,0);
+  //first 2 using pointers as parameters
+  vector<int> v3(v1.begin(), v1.begin() + 2);
+
+  cout << "Last item: " << v1[2] << endl;
+  v1.push_back(4);
+  cout << "Last item: " << v1[3] << endl;
+  // possible but not the best practice.
+  cout << "One past Last item: " << v1[7] << endl;
+
+  printVector(v1);
+  printVector(v2);
+  printVector(v3);
+
   return 0;
 }
