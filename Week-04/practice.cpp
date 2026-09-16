ad37a6164e1a43158dd552bc66e12d783de87492
jhonatanparada499 <jhonatanparada499@gmail.com>
2026-09-16 09:09:08 -0400

    feat: add capacity, insert and erase mem funcs

--- Week-04/practice.cpp
+++ Week-04/practice.cpp
@@ -6,7 +6,18 @@ using namespace std;
 int main(){
   vector<int> v2(5,0);
   vector<int> v1 = {2,1,3};
-  sort(v1.begin(), v1.end());
+  // sort(v1.begin(), v1.end());
+
+  // slower performance to remove or add elements in middle
+  v1.insert(v1.begin() + 2, 5);
+  v1.erase(v1.begin() + 2);
+
+  // reverse(v1.begin(), v1.end());
+  //
+  // v1.clear();
+  //
+  v1.reserve(4);
+  cout << v1.capacity() << endl;
 
   for (auto it=v1.begin(); it!=v1.end(); ++it){
     cout << *it << " ";
